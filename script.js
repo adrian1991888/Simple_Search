abe1f8fa584528a96ac81a8871995cad48deae40
adriankanski <108273183+adriankanski@users.noreply.github.com>
2023-09-01 17:21:45 +0200

    add repo/files

---
+++ script.js
@@ -0,0 +1,32 @@
+//pobranie elementów
+
+const inputElement = document.querySelector('.search')
+const drinkList = document.querySelector('.drink-list')
+const li = document.querySelectorAll('li')
+const ul = document.querySelector('ul')
+const btn = document.querySelector('.btn')
+
+
+
+
+//funkcja sprawdzająca czy ciąg znaków wpisany przez usera zawiera się w liście napojów
+
+function search(e){
+    e = inputElement.value
+for (let i = 0; i < li.length; i++) {
+    if (li[i].textContent.includes(e)) {
+        li[i].classList.remove('hidden')
+    }else if(inputElement.value == null){
+        li[i].classList.remove('hidden')
+    }else{
+        li[i].classList.add('hidden')
+    }
+}
+}
+
+
+//po puszczeniu klawisza wykonuje funkcję search
+
+inputElement.addEventListener("keyup", search)
+
+
